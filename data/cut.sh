8cba950d3495b75dee2c4282f688f488124775c4
Kunichika Tsumoto <42426029+92tsumoto@users.noreply.github.com>
2018-11-12 17:16:29 +0900

    Add files via upload

---
+++ data/cut.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# PL-gnu.sh
+
+cat data.out | awk '{print $1,$2}' > ap.dat
+cat data.out | awk '{print $1,$3}' > cai.dat
+cat data.out | awk '{print $1,$4}' > nai.dat
+cat data.out | awk '{print $1,$5}' > ina.dat
+cat data.out | awk '{print $1,$6}' > ical.dat
+cat data.out | awk '{print $1,$7}' > ito.dat
+cat data.out | awk '{print $1,$8}' > ikr.dat
+cat data.out | awk '{print $1,$9}' > iks.dat
+cat data.out | awk '{print $1,$10}' > ik1.dat
+cat data.out | awk '{print $1,$11}' > inak.dat
+cat data.out | awk '{print $1,$12}' > incx.dat
+cat data.out | awk '{print $1,$13}' > inab.dat
+cat data.out | awk '{print $1,$14}' > icab.dat
+cat data.out | awk '{print $1,$15}' > ipca.dat
+cat data.out | awk '{print $1,$16}' > isitm.dat
